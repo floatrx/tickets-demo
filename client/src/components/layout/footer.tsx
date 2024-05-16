cc4dd6d2b3ace4ce90704ab6646dd04b4355935f
Andrew floatrx <ifloatrx@gmail.com>
2024-05-16 15:32:46 +0300

    update footer (client)

---
+++ client/src/components/layout/footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => (
+  <footer className="border-t">
+    <div className="container p-4 text-xs text-gray-400 text-center">
+      <p>
+        © 2024 AviaSales {' | '}
+        <a href="https://github.com/floatrx/tickets-demo" target="_blank" rel="noopener" className="text-blue-500">
+          GitHub.
+        </a>
+      </p>
+    </div>
+  </footer>
+);
--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/layout/header';
+import { Footer } from '@/components/layout/footer';
 
 import type { PropsWithChildren } from 'react';
 
@@ -6,15 +7,6 @@ export const Layout = (props: Required<PropsWithChildren>) => (
   <div className="flex min-h-screen flex-col">
     <Header />
     <main className="container my-8 flex-1">{props.children}</main>
-    <footer className="border-t">
-      <div className="container p-4 text-xs text-gray-400 text-center">
-        <p>
-          © 2024 AviaSales {' | '}
-          <a href="https://github.com/floatrx/resp-aviasales" target="_blank" rel="noopener" className="text-blue-500">
-            GitHub.
-          </a>
-        </p>
-      </div>
-    </footer>
+    <Footer />
   </div>
 );
